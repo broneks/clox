4f8d1b7d334371ff41f924a14d4a31c305fd9666
Bronek Szulc <bronek@fastmail.fm>
2023-04-06 21:08:21 -0400

    arithmetic

--- debug.c
+++ debug.c
@@ -3,7 +3,7 @@
 #include "debug.h"
 #include "value.h"
 
-static int simpleInteraction(const char* name, int offset) {
+static int simpleInstruction(const char* name, int offset) {
   printf("%s\n", name);
   return offset + 1;
 }
@@ -38,11 +38,23 @@ int disassembleInstruction(Chunk* chunk, int offset) {
     case OP_CONSTANT:
       return constantInstruction("OP_CONSTANT", chunk, offset);
 
+    case OP_ADD:
+      return simpleInstruction("OP_ADD", offset);
+
+    case OP_SUBTRACT:
+      return simpleInstruction("OP_SUBTRACT", offset);
+
+    case OP_MULTIPLY:
+      return simpleInstruction("OP_MULTIPLY", offset);
+
+    case OP_DIVIDE:
+      return simpleInstruction("OP_DIVIDE", offset);
+
     case OP_NEGATE:
-      return simpleInteraction("OP_NEGATE", offset);
+      return simpleInstruction("OP_NEGATE", offset);
 
     case OP_RETURN:
-      return simpleInteraction("OP_RETURN", offset);
+      return simpleInstruction("OP_RETURN", offset);
 
     default:
       printf("Unknown opcode %d\n", instruction);
