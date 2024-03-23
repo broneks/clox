5da12d2b7faa3231ccfa8a5e53738b0146bdc0fc
Bronek Szulc <bronek@fastmail.fm>
2024-03-23 14:07:17 -0400

    broken function impl

--- compiler.c
+++ compiler.c
@@ -44,7 +44,14 @@ typedef struct {
   int depth;
 } Local;
 
+typedef enum {
+  TYPE_FUNCTION,
+  TYPE_SCRIPT
+} FunctionType;
+
 typedef struct {
+  ObjFunction* function;
+  FunctionType type;
   Local locals[UINT8_COUNT];
   int localCount;
   int scopeDepth;
@@ -52,9 +59,9 @@ typedef struct {
 
 Parser parser;
 Compiler* current = NULL;
-Chunk* compilingChunk;
+
 static Chunk* currentChunk() {
-  return compilingChunk;
+  return &current->function->chunk;
 }
 
 static void errorAt(Token* token, const char* message) {
@@ -172,19 +179,31 @@ static void patchJump(int offset) {
   currentChunk()->code[offset + 1] = jump & 0xff;
 }
 
-static void initCompiler(Compiler* compiler) {
+static void initCompiler(Compiler* compiler, FunctionType type) {
+  compiler->function = NULL;
+  compiler->type = type;
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
+  compiler->function = newFunction();
   current = compiler;
+
+  Local* local = &current->locals[current->localCount++];
+  local->depth = 0;
+  local->name.start = "";
+  local->name.length = 0;
 }
 
-static void endCompiler() {
+static ObjFunction* endCompiler() {
   emitReturn();
+  ObjFunction* function = current->function;
+
 #ifdef DEBUG_PRINT_CODE
   if (!parser.hadError) {
-    disassembleChunk(currentChunk(), "code");
+    disassembleChunk(currenChunk(), function->name != NULL ? function->name->chars : "<script>");
   }
 #endif
+
+  return function;
 }
 
 static void beginScope() {
@@ -193,7 +212,7 @@ static void beginScope() {
 
 static void endScope() {
   current->scopeDepth--;
-  
+
   while (current->localCount > 0 && current->locals[current->localCount - 1].depth > current->scopeDepth) {
     emitByte(OP_POP);
     current->localCount--;
@@ -621,11 +640,10 @@ static void statement() {
   }
 }
 
-bool compile(const char* source, Chunk* chunk) {
+ObjFunction* compile(const char* source) {
   initScanner(source);
   Compiler compiler;
-  initCompiler(&compiler);
-  compilingChunk = chunk;
+  initCompiler(&compiler, TYPE_SCRIPT);
 
   parser.hadError = false;
   parser.panicMode = false;
@@ -636,7 +654,7 @@ bool compile(const char* source, Chunk* chunk) {
     declaration();
   }
 
-  endCompiler();
+  ObjFunction* function = endCompiler();
 
-  return !parser.hadError;
+  return parser.hadError ? NULL : function;
 }
--- object.h
+++ object.h
@@ -2,23 +2,34 @@
 #define clox_object_h
 
 #include "common.h"
+#include "chunk.h"
 #include "value.h"
 
 #define OBJ_TYPE(value)  (AS_OBJ(value)->type)
 
+#define IS_FUNCTION(value) isObjectType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjectType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value) ((ObjFunction*)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString*)AS_OBJ(value))->chars)
 
 typedef enum {
-  OBJ_STRING,
+  OBJ_FUNCTION,
+  OBJ_STRING
 } ObjType;
 
 struct Obj {
   ObjType type;
   struct Obj* next; // linked list
 };
+
+typedef struct {
+  Obj obj; // pseudo-inheritance / base class
+  int arity;
+  Chunk chunk;
+  ObjString* name;
+} ObjFunction;
 
 /*
  * Given an ObjString* you can safely cast it to Obj* and then access the type field from it.
@@ -33,6 +44,7 @@ struct ObjString {
   uint32_t hash;
 };
 
+ObjFunction* newFunction();
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char*  chars, int length);
 void printObject(Value value);
