447d4caa5374d2e8361023b4ac586ed6f1663d38
Bronek Szulc <bronek@fastmail.fm>
2024-10-06 16:36:22 -0400

    closures (479)

--- chunk.h
+++ chunk.h
@@ -13,8 +13,10 @@ typedef enum {
   OP_GET_LOCAL,
   OP_SET_LOCAL,
   OP_GET_GLOBAL,
+  OP_GET_UPVALUE,
   OP_DEFINE_GLOBAL,
   OP_SET_GLOBAL,
+  OP_SET_UPVALUE,
   OP_EQUAL,
   OP_GREATER,
   OP_LESS,
@@ -29,6 +31,7 @@ typedef enum {
   OP_JUMP_IF_FALSE,
   OP_LOOP,
   OP_CALL,
+  OP_CLOSURE,
   OP_RETURN,
 } OpCode;
 
